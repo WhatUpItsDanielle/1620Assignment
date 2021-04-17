125f591ba9c06c151134bac7f1c44a51e7be1e1a
WhatUpItsDanielle <danielle.davidson98@gmail.com>
2021-04-16 23:39:15 -0700

    .js is linked to index.html and general outline of javascript is in the .hs file in plain text

---
+++ Javascript/Script.js
@@ -0,0 +1,18 @@
+Dark Theme to light theme:
+* define dark Theme class (includes button change)
+* define light theme class (includes button change)
+
+*add event listener for dark theme button to be clicked(when clicked triggers the switching of themes)
+
+
+
+Hiding notepad and making it re-apear:
+* define hidden class (make cancel button, save button and notepad invisable)
+* difine show class (make cancel button, save button and notepad show up)
+
+*add event listener so that when cancel button is clicked the hidden class is called
+*add event listener so that when new note button is pressed the show class is called
+
+
+
+
